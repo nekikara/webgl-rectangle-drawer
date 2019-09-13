bd4620bb210a477122fab83ccf6efa7cc4d90104
nekikara <nekikara@gmail.com>
2019-09-13 20:05:23 +0200

    Introduce babylon.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
 function main() {
-    const canvas = document.getElementById("glCanvas");
-    canvas.width = 640;
-    canvas.height = 480;
-    const canvasRect = canvas.getBoundingClientRect();
-    const halfCanvasX = canvas.width / 2;
-    const halfCanvasY = canvas.height / 2;
+    const canvas = document.getElementById("renderCanvas");
+    const engine = new BABYLON.Engine(canvas, true);
 
-    // Add the event listener for mousemove
-    canvas.addEventListener('mousemove', e => {
-        const canvasX = e.clientX - canvasRect.x;
-        const canvasY = e.clientY - canvasRect.y;
+    const createScene = () => {
+        const scene = new BABYLON.Scene(engine);
 
-        const glX = (canvasX - halfCanvasX) / halfCanvasX;
-        const glY = -1 * (canvasY - halfCanvasY) / halfCanvasY;
+        const camera = new BABYLON.UniversalCamera("Camera", new BABYLON.Vector3(0, 10, 0), scene);
+        camera.setTarget(BABYLON.Vector3.Zero());
+
+
+        // Light
+        const light1 = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(100, 1000, 1000), scene);
+
+        // Shape
+        const ground = BABYLON.MeshBuilder.CreateGround("ground1", {width: 10, height: 5}, scene);
+        ground.material = new BABYLON.StandardMaterial('groundMat', scene);
+        ground.material.diffuseColor = new BABYLON.Color3(0.9, 0.5, 0.5);
+
+        scene.onPointerMove = (evt, pickInfo, type) => {
+            console.log(scene.pointerX, scene.pointerY);
+            const pInfo = scene.pick(scene.pointerX, scene.pointerY);
+            console.log(pInfo);
+        };
+        console.log('camera', camera.getWorldMatrix());
+
+        return scene;
+    };
+
+    const scene = createScene();
+    engine.runRenderLoop(() => {
+        scene.render();
     });
 
-    const gl = canvas.getContext('webgl');
-
-    if (gl === null) {
-        alert("Unable to initialize WebGL.");
-        return;
-    }
-
-    gl.clearColor(0.0, 0.0, 0.0, 0.3);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    window.addEventListener('resize', () => {
+        engine.resize();
+    });
 }
 
-main();
+window.addEventListener('DOMContentLoaded', () => {{
+    main();
+}});
