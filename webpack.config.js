6fb05536a4354befc9c740dd8c8c08d4245342c5
nekikara <nekikara@gmail.com>
2019-09-21 18:14:23 +0200

    some plugins to ignore dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/js/main.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
