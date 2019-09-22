8a20cadb5bfbc8e4701630dccc093d3d0a461121
nekikara <nekikara@gmail.com>
2019-09-22 17:54:18 +0200

    Draw webgl canvas

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,14 +1,29 @@
 import '../scss/main.scss';
 
-type X = {
-  id: string
+function getWebGLContext(canvas: HTMLElement) {
+  // Get the rendering context for WebGL
+  const gl = (canvas as HTMLCanvasElement).getContext('webgl');
+  console.log(gl);
+  return gl;
 }
 
 function main() {
-  const x: X = {id: "hogehoeg"};
-  console.log('xxxx', x);
-}
+  // Retrieve <canvas> element
+  const canvas: HTMLElement = document.getElementById('renderCanvas');
 
+  // Get the rendering context for WebGL
+  const gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Set clear color
+  gl.clearColor(0.5, 0.5, 0.9, 1.0);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
 
 window.addEventListener('DOMContentLoaded', () => {{
     main();
