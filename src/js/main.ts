4f0f68eaa1a6d1605f48206f3e096c1c8aa6e433
nekikara <nekikara@gmail.com>
2019-09-24 13:08:08 +0200

    Draw rectangles with two points clicked

--- src/js/main.ts
+++ src/js/main.ts
@@ -51,16 +51,37 @@ window.addEventListener('DOMContentLoaded', () => {{
   main();
 }});
 
-const positions: number[] = [];
+type Position = {
+  x: number,
+  y: number
+}
+const toRectPositions = (pos: Position[]): number[] => {
+  const first = pos[0];
+  const second = pos[1];
+  const v = [first.x, first.y];
+  v.push(first.x);
+  v.push(second.y);
+  v.push(second.x);
+  v.push(first.y);
+  v.push(second.x);
+  v.push(second.y);
+  return v;
+};
+let positions: Position[] = [];
+let vs: number[] = [];
 const drawPoints = (ev: MouseEvent, gl: WebGLRenderingContext, canvas: HTMLCanvasElement, program: WebGLProgram) => {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
   const rect = canvas.getBoundingClientRect() as DOMRect;
   const glX = ((ev.clientX - rect.x) - (rect.width / 2)) / (rect.width / 2);
   const glY = -((ev.clientY - rect.y) - (rect.height / 2)) / (rect.height / 2);
-  positions.push(glX);
-  positions.push(glY);
-  const vertices = new Float32Array(positions);
+  positions.push({x: glX, y: glY});
+  if (positions.length % 2 !== 0) {
+    return;
+  }
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  vs = vs.concat(toRectPositions(positions));
+  positions = [];
+  const vertices = new Float32Array(vs);
   const vertexBuffer = gl.createBuffer();
   if (!vertexBuffer) {
     console.error('Failed to create the buffer object');
@@ -76,7 +97,10 @@ const drawPoints = (ev: MouseEvent, gl: WebGLRenderingContext, canvas: HTMLCanva
   }
   gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(a_Position);
-  gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2);
+
+  for (let i=0; i < (vertices.length / 8); i++) {
+    gl.drawArrays(gl.TRIANGLE_STRIP, i * 4, 4);
+  }
 };
 
 // Util functions
