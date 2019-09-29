fd622d5c49f8e15408dce608957191822a1d1ad9
nekikara <nekikara@gmail.com>
2019-09-29 21:18:12 +0200

    Color Picker

--- src/js/main.ts
+++ src/js/main.ts
@@ -11,63 +11,29 @@ void main() {
 
 const FSHADER_SOURCE = `
 precision mediump float;
+#define TWO_PI 6.28318530718
 uniform vec2 uResolution;
 uniform vec2 uFirstPos;
 uniform vec2 uSecondPos;
 
-float desk(float min, float max, float x) {
-  if (min <= x && x <= max) {
-    return 1.;
-  }
-  return 0.;
-}
-
-vec3 colorA = vec3(255., 0., 0.) / 255.0;
-vec3 colorB = vec3(255., 127., 0.) / 255.0;
-vec3 colorC = vec3(255., 255., 0.) / 255.0;
-vec3 colorD = vec3(0., 255., 0.) / 255.0;
-vec3 colorE = vec3(0., 0., 255.) / 255.0;
-vec3 colorF = vec3(75., 0., 130.) / 255.0;
-vec3 colorG = vec3(148., 0., 211.) / 255.0;
 vec3 white = vec3(1.);
-
-vec3 selectColor(int index) {
-  if (index == 0) {
-    return colorA;
-  } else if (index == 1) {
-    return colorB;
-  } else if (index == 2) {
-    return colorC;
-  } else if (index == 3) {
-    return colorD;
-  } else if (index == 4) {
-    return colorE;
-  } else if (index == 5) {
-    return colorF;
-  } else if (index == 6) {
-    return colorG;
-  } else {
-    return white;
-  }
-}
 
 int remain(int numerator, int denominator) {
   int ans = numerator / denominator;
   return numerator - ans * denominator;
 }
 
-void main() {
-  vec3 colors[8];
-  colors[0] = colorA;
-  colors[1] = colorB;
-  colors[2] = colorC;
-  colors[3] = colorD;
-  colors[4] = colorE;
-  colors[5] = colorF;
-  colors[6] = colorG;
-  colors[7] = white;
+vec3 hsb2rgb( in vec3 c ){
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
+                             6.0)-3.0)-1.0,
+                     0.0,
+                     1.0 );
+    rgb = rgb*rgb*(3.0-2.0*rgb);
+    return c.z * mix( vec3(1.0), rgb, c.y);
+}
 
-  const vec2 center = vec2(0.5, 0.3);
+void main() {
+  const vec2 center = vec2(0.5);
 
   // Map coordinate to specified rectangle
   float minX = min(uFirstPos.x, uSecondPos.x);
@@ -77,22 +43,14 @@ void main() {
   float width = abs(minX - maxX);
   float height = abs(minY - maxY);
   vec2 rst = vec2(gl_FragCoord.x - minX,  gl_FragCoord.y - minY) / vec2(width, height);
-
-  float dis = distance(rst, center);
-  vec3 background = vec3(0.);
-  // X axis
-  float stp = 1.0 / 8.0;
-  float indexX = 0.;
-  for (int i=0; i<8; i++) {
-    indexX += step(stp * float(i + 1), rst.x);
-  }
-  float indexY = 0.;
-  for (int i=0; i<8; i++) {
-    indexY += step(stp * float(i + 1), rst.y);
-  }
   
-  background = selectColor(remain(int(indexX + indexY), 8));
+  vec2 c = rst - vec2(0.5);
+  float angle = atan(c.y, c.x);
+  float radius = distance(rst, center);
+  float h = 1.- pow(sin((TWO_PI / 2.) * (angle / TWO_PI  - 1.5) / 2. ), 1.5);
+  vec3 colorA = hsb2rgb(vec3( h, radius * 2., 1.0 ));
   
+  vec3 background = mix(colorA, white, step(0.45, radius));
   gl_FragColor = vec4(background, 1.);
 }
 `;
