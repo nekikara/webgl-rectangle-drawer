8eb64f9463caec4d5474c31e0962dd1d96970be2
nekikara <nekikara@gmail.com>
2019-09-29 14:24:39 +0200

    Draw colorful map

--- src/js/main.ts
+++ src/js/main.ts
@@ -31,13 +31,41 @@ vec3 colorF = vec3(75., 0., 130.) / 255.0;
 vec3 colorG = vec3(148., 0., 211.) / 255.0;
 vec3 white = vec3(1.);
 
+vec3 selectColor(int index) {
+  if (index == 0) {
+    return colorA;
+  } else if (index == 1) {
+    return colorB;
+  } else if (index == 2) {
+    return colorC;
+  } else if (index == 3) {
+    return colorD;
+  } else if (index == 4) {
+    return colorE;
+  } else if (index == 5) {
+    return colorF;
+  } else if (index == 6) {
+    return colorG;
+  } else {
+    return white;
+  }
+}
+
+int remain(int numerator, int denominator) {
+  int ans = numerator / denominator;
+  return numerator - ans * denominator;
+}
+
 void main() {
-  vec3 colors[5];
+  vec3 colors[8];
   colors[0] = colorA;
   colors[1] = colorB;
   colors[2] = colorC;
   colors[3] = colorD;
-  colors[4] = white;
+  colors[4] = colorE;
+  colors[5] = colorF;
+  colors[6] = colorG;
+  colors[7] = white;
 
   const vec2 center = vec2(0.5, 0.3);
 
@@ -51,15 +79,19 @@ void main() {
   vec2 rst = vec2(gl_FragCoord.x - minX,  gl_FragCoord.y - minY) / vec2(width, height);
 
   float dis = distance(rst, center);
-  vec3 background = white;
-  background = mix(background, colorG, desk(0.3, 0.33, dis));
-  background = mix(background, colorF, desk(0.33, 0.36, dis));
-  background = mix(background, colorE, desk(0.36, 0.39, dis));
-  background = mix(background, colorD, desk(0.39, 0.42, dis));
-  background = mix(background, colorC, desk(0.42, 0.45, dis));
-  background = mix(background, colorB, desk(0.45, 0.48, dis));
-  background = mix(background, colorA, desk(0.48, 0.51, dis));
-  background = mix(white, background, step(center.y, rst.y));
+  vec3 background = vec3(0.);
+  // X axis
+  float stp = 1.0 / 8.0;
+  float indexX = 0.;
+  for (int i=0; i<8; i++) {
+    indexX += step(stp * float(i + 1), rst.x);
+  }
+  float indexY = 0.;
+  for (int i=0; i<8; i++) {
+    indexY += step(stp * float(i + 1), rst.y);
+  }
+  
+  background = selectColor(remain(int(indexX + indexY), 8));
   
   gl_FragColor = vec4(background, 1.);
 }
